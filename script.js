1f0b5d4fac68f804964f196fb4bebd40f2cd82c4
Karna Jani <karna.jani@gmail.com>
2023-10-30 15:53:34 -0700

    Added openly available username and password as basic auth. sending query params instead of a post request body.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 $('#my-button').on('click', async function() {
     const inputVal = $('#my-input').val();
+    const username = 'kids.karnajani.com';
+    const password = 'asfiuhwtu1249!!';
 
     try {
         const response = await $.ajax({
             url: 'https://n8n.minervasummary.chat/webhook/18f3768f-4443-436f-b630-caf6e507bab7',
             type: 'GET',
-            contentType: 'application/json',
-            data: JSON.stringify({ instruction: inputVal }),
+            headers: {
+                'Authorization': 'Basic ' + btoa(username + ':' + password)
+            },
+            data: { instruction: inputVal }, // Pass the data as an object
         });
 
         $('#response-image').attr('src', response.url);

4cfe3ef78a87cfc2602622896c6f54bff2add588
Karna Jani <karna.jani@gmail.com>
2023-10-30 15:08:29 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$('#my-button').on('click', async function() {
+    const inputVal = $('#my-input').val();
+
+    try {
+        const response = await $.ajax({
+            url: 'https://n8n.minervasummary.chat/webhook/18f3768f-4443-436f-b630-caf6e507bab7',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ instruction: inputVal }),
+        });
+
+        $('#response-image').attr('src', response.url);
+        $('#response-image-container').show();
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+});

9437c1d6b3eb705ce7622d7c8571915b7bd14e3e
Karna Jani <karna.jani@gmail.com>
2023-10-31 11:54:21 -0700

    removed basic auth from ajax call

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 $('#my-button').on('click', async function() {
     const inputVal = $('#my-input').val();
-    const username = 'kids.karnajani.com';
-    const password = 'asfiuhwtu1249!!';
 
     try {
         const response = await $.ajax({
             url: 'https://n8n.minervasummary.chat/webhook/18f3768f-4443-436f-b630-caf6e507bab7',
             type: 'GET',
-            headers: {
-                'Authorization': 'Basic ' + btoa(username + ':' + password)
-            },
             data: { instruction: inputVal }, // Pass the data as an object
         });
 
